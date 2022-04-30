8e8def1c501e0dfdc674ed0612df0afc5b39e50c
cwastche <marvin@42.fr>
2022-04-30 15:00:26 +0200

    norm

--- srcs/parsing/check_params.c
+++ srcs/parsing/check_params.c
@@ -33,7 +33,7 @@ static int	fill_color(char *str, int *color)
 		j = -1;
 		while (tmp[i][++j])
 			if (!ft_isdigit(tmp[i][j]))
-				return (write(2, "Error\nForbidden char detected\n", 30), 1);
+				return (write(2, "Error\nForbidden char detected\n", 30), 1); //PROBLEM HERE I THINK
 		if (ft_atoi(tmp[i]) < 0 || ft_atoi(tmp[i]) > 255)
 			return (write(2, "Error\nWrong color value\n", 24), 1);
 	}

9fc1029759751d53ee622c383d1777f33989f30f
cwastche <marvin@42.fr>
2022-05-02 21:58:54 +0200

    WFH

--- srcs/parsing/check_walls.c
+++ srcs/parsing/check_walls.c
@@ -93,18 +93,24 @@ static int	check_last_line(char **wall, int i)
 	{
 		if (is_space(wall[i][j]))
 		{
-			// if (wall[i][j + 1] != '1' && !is_space(wall[i][j + 1])
-			// 	&& wall[i][j + 1] != 0 && wall[i][j + 1] != EOF)
-			// 	return (1);
-			if (len >= j)
-				if (wall[i - 1][j] != '1' && !is_space(wall[i - 1][j])
-					&& wall[i - 1][j] != 0 && wall[i - 1][j] != EOF)
-					return (1);
-			if (len < j && wall[i][j] == '0')
+			if (wall[i][j + 1] != '1' && !is_space(wall[i][j + 1])
+				&& wall[i][j + 1] != 0 && wall[i][j + 1] != EOF)
 				return (1);
+			// if (len >= j)
+			// 	if (wall[i - 1][j] != '1' && !is_space(wall[i - 1][j])
+			// 		&& wall[i - 1][j] != 0 && wall[i - 1][j] != EOF)
+			// 		return (1);
+			// if (len < j && wall[i][j] == '0')
+			// 	return (1);
 		}
 		else if (wall[i][j] != '1')
 			return (1);
+		else if (len > j)
+		{
+			if (wall[i - i][j] != '1')
+				return (1);
+			j++;
+		}
 	}
 	return (0);
 }

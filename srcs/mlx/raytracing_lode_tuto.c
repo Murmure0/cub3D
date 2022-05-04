b303b38c314897e5ea898464c9dfde8f2ee30448
cwastche <marvin@42.fr>
2022-05-04 19:19:08 +0200

    WIP

--- srcs/mlx/raytracing_lode_tuto.c
+++ srcs/mlx/raytracing_lode_tuto.c
@@ -99,23 +99,11 @@ void	raytracing(t_player *player, t_file *file, t_mlx *mlx)
 				mapY += stepY;
 				side = 1;
 			}
-<<<<<<< HEAD
-			if (mapY >= 0 && mapY < distanceMax
-				&& mapX >= 0
-				&& mapX < distanceMax )
-				{
-					if (file->scene[mapY][mapX] == '1')
-					{
-						hit = 1;
-					}
-				}
-=======
 			//check 
 			if (file->scene[mapY][mapX +stepX] == '1' ||file->scene[mapY + stepY][mapX] == '1')
 			{
 				hit = 1;
 			}
->>>>>>> 658afae44a718cb0e65d50b29b43b0a2e60e7008
 		}
 				//printf("MAP COORDS x %c\n, x%d, y%d\n", file->scene[mapY][mapX], mapX, mapY);
 
